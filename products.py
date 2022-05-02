9781c46c0b2c18c5af91c4060fde0de077d3909f
phoebe <a0909309298@gmail.com>
2022-05-02 11:04:01 +0800

    pd

--- products.py
+++ products.py
@@ -1,7 +1,17 @@
+proudcts = []
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
 		break
 	price = input ('請輸入商品價格:')
-	produsts.append([name,price])
+	price = int(price)
+	proudcts.append([name,price])
 print(proudcts)
+
+for p in products:
+	print(P[0],'的價格是',P[1])
+
+with open ('products.cvs','w',encoding='utf-8') as f:
+	f.write('商品,價格')
+	for p in products:
+		f.write(p[0] + ','+ p[1] + '\n')
